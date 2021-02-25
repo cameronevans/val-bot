a9aa61bd4f5aa67f2e0c4fb3d434c28457bec469
unknown <cam.abevans@gmail.com>
2021-02-25 18:27:20 -0400

    added commands, hopefully isn't broken

--- index.js
+++ index.js
@@ -1,21 +1,71 @@
-const { exec } = require("child_process");
-const Discord = require("discord.js");
+const { exec } = require('child_process');
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const commands = ["!help", "!status", "!usage", "!start", "!stop", "!restart"];
-const confirmationMessages = [
-  "One moment please..",
-  "You got it...",
-  "Just one sec...",
-  "Sure thing bud...",
-  "*beeps affirmatively*...",
-  "Where's server higgins...",
+const VHSERVER = '/home/vhserver/vhserver';
+const helpCommand = '!help';
+const helpAdminCommand = '!help-admin';
+const statusCommand = '!status';
+const usageCommand = '!usage';
+const startCommand = '!start';
+const stopCommand = '!stop';
+const restartCommand = '!restart';
+const netstatCommand = '!netstat';
+const validateCommand = '!validate';
+const backupCommand = '!backup';
+const listBackupCommand = '!list-backup';
+const updateCommand = '!update';
+const updateLGSMCommand = '!update-lgsm';
+
+const commands = [
+  helpCommand,
+  helpVerbose,
+  helpAdminCommand,
+  statusCommand,
+  usageCommand,
+  startCommand,
+  stopCommand,
+  restartCommand,
 ];
-const grepDetails = (grep) =>
-  `/home/vhserver/vhserver dt | ${grep} | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g"`;
+const adminCommands = [
+  netstatCommand,
+  validateCommand,
+  backupCommand,
+  updateCommand,
+  updateLGSMCommand,
+];
+
+const verbose = {
+  [helpCommand]: 'lists basic commands',
+  [helpAdminCommand]: 'lists admin commands',
+  [statusCommand]: 'displays server online status & details',
+  [usageCommand]: 'displays server current CPU/Memory/Storage usage',
+  [startCommand]: 'starts the valheim server',
+  [stopCommand]: 'stops the valheim server gracefully',
+  [restartCommand]: 'restarts the valheim server',
+};
+const confirmationMessages = [
+  'One moment please...',
+  'You got it...',
+  'Just one sec...',
+  'Sure thing bud...',
+  '*beeps affirmatively*...',
+  "Where's server higgins...",
+  'Hoy...',
+  '*beeping intensifies*...',
+  'Coming right up...',
+  'Roger Roger...',
+  `Yes m'lord...`,
+  `Aye-aye...`,
+  'Wololo...',
+  'Affirmative...',
+];
+const vhserverCommand = (command) => `${VHSERVER} ${command}`;
+const grepDetails = (grep, lines = 8) =>
+  `/home/vhserver/vhserver dt | grep -A ${lines} '${grep}' | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g"`;
 const formatter = (message) => `\`\`\`\n${message}\n\`\`\``;
 
-client.on("message", (message) => {
+client.on('message', (message) => {
   const confirmMessage = () =>
     message.channel.send(
       confirmationMessages[
@@ -34,38 +84,81 @@ client.on("message", (message) => {
     message.channel.send(formatter(stdout));
   };
 
-  switch (message.content) {
-    case "!help":
-      message.channel
-        .send(`Hi! I can communicate to the Valheim server for you.\n Commands: \`${commands.join(
-        "`, `"
-      )}\`
-      `);
-      break;
-    case "!status":
-      exec(grepDetails(`grep -A 8 'Valheim Server Details'`), cb);
-      break;
-    case "!usage":
-      exec(grepDetails(`grep -A 8 'Game Server Resource Usage'`), cb);
-      break;
-    case "!start":
-      confirmMessage();
-      exec("/home/vhserver/vhserver start", cb);
-      break;
-    case "!stop":
-      confirmMessage();
-      exec("/home/vhserver/vhserver stop", cb);
-      break;
-    case "!restart":
-      confirmMessage();
-      exec("/home/vhserver/vhserver restart", cb);
-      break;
-    default:
+  if ([...commands, ...adminCommands].includes(message.content)) {
+    switch (message.content) {
+      case '!help':
+        message.channel
+          .send(`Hi! I can communicate with the Valheim server for you.\nCommands: \`${commands.join(
+          '`, `'
+        )}\`
+        `);
+        break;
+      case '!help-verbose':
+        message.channel
+          .send(`Hi! I can communicate with the Valheim server for you.\nCommands: \`${verbose.map(
+          (command, description) => `\`${command}\`: ${description}\n`
+        )}\`
+        `);
+        break;
+      case '!help-admin':
+        message.channel.send(
+          `Admin commands **use at your own risk**: ${adminCommands.join(
+            '`, `'
+          )}\``
+        );
+        break;
+      case '!status':
+        confirmMessage();
+        exec(grepDetails('Valheim Server Details'), cb);
+        break;
+      case '!usage':
+        confirmMessage();
+        exec(grepDetails('Game Server Resource Usage'), cb);
+        break;
+      case '!start':
+        confirmMessage();
+        exec(vhserverCommand('start'), cb);
+        break;
+      case '!stop':
+        confirmMessage();
+        exec(vhserverCommand('stop'), cb);
+        break;
+      case '!restart':
+        confirmMessage();
+        exec(vhserverCommand('restart'), cb);
+        break;
+      case '!netstat':
+        confirmMessage();
+        exec('netstat -atunp | grep valheim', cb);
+        break;
+      case '!validate':
+        confirmMessage();
+        exec(vhserverCommand('validate'), cb);
+        break;
+      case '!check-update':
+        confirmMessage();
+        exec(vhserverCommand('check-update'), cb);
+        break;
+      case '!update':
+        confirmMessage();
+        exec(vhserverCommand('update'), cb);
+        break;
+      case '!list-backup':
+        confirmMessage();
+        exec(grepDetails('Backups'), 4, cb);
+        break;
+      case '!backup':
+        confirmMessage();
+        exec(vhserverCommand('backup'), cb);
+        break;
+
+      default:
+    }
   }
 });
 
-client.once("ready", () => {
-  console.log("Ready!");
+client.once('ready', () => {
+  console.log('val-bot is online!');
 });
 
-client.login("ODE0MjUxOTc1NjgzMjExMjY1.YDbJPw.T88DsNiPZJ-W9cIsLPQvEOHOG8Y");
+client.login('ODE0MjUxOTc1NjgzMjExMjY1.YDbJPw.T88DsNiPZJ-W9cIsLPQvEOHOG8Y');
