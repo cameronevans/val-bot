86f6f6669cb926e9543f9f63c03021354d800964
unknown <cam.abevans@gmail.com>
2021-02-24 23:18:18 -0400

    status command

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
+const { exec } = require('child_process');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('message', message => {
-  if (message.content === '!hello') {
-      message.channel.send('Hello Val!');
+  let response = 'Something broke! Blame @smingers';
+  if (message.content === '!status') {
+      exec(`/home/vhserver dt | grep -A 8 'Valheim Server Details'`, (error, stdout, stderr) => {
+        if (error) {
+            message.channel.send(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            message.channel.send(`stderr: ${stderr}`);
+            return;
+        }
+        message.channel.send(response);
+    });
   }
 });
 
 client.once('ready', () => {
   console.log('Ready!');
-}
+});
 
-client.login('f21ce7a09a7ed89851b79102c24d653209987dfa6aaf6e296e6eb68038652917')
+client.login('ODE0MjUxOTc1NjgzMjExMjY1.YDbJPw.T88DsNiPZJ-W9cIsLPQvEOHOG8Y')
